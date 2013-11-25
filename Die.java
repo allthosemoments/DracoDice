953c0cd41bc86f7f04f5603f7d3a77871c4785a1
Jared <allthosemoments@gmail.com>
2013-11-25 15:03:37 -0600

    gold/silver/bronze dice now weighted

--- Die.java
+++ Die.java
@@ -2,7 +2,8 @@ import java.util.Random;
 
 public class Die
 {
-	public enum type { GOLD, SILVER, BRONZE }
+	public enum type   { GOLD, SILVER, BRONZE }
+	public enum action { BURN, CONT, LOOT}
 
 	private type _treasure;
 
@@ -13,7 +14,7 @@ public class Die
 
 	public Die(char treasure)
 	{
-		switch(treasure)
+		switch( treasure )
 		{
 		case 'g': _treasure = type.GOLD;   break;
 		case 's': _treasure = type.SILVER; break;
@@ -23,6 +24,25 @@ public class Die
 
 	public int roll()
 	{
-		return 1 + (int)(6 * Math.random());
+		int d6 = 1 + (int)(6 * Math.random());
+		switch( _treasure )
+		{
+		case GOLD:
+			if     (d6 < 2) return 0;
+			else if(d6 < 5) return 1;
+			else if(d6 < 7) return 2;
+			break;
+		case SILVER:
+			if     (d6 < 3) return 0;
+			else if(d6 < 5) return 1;
+			else if(d6 < 7) return 2;
+			break;
+		case BRONZE:
+			if     (d6 < 4) return 0;
+			else if(d6 < 6) return 1;
+			else if(d6 < 7) return 2;
+			break;
+		}
+		return -1;
 	}
 }
--- DracoDice.java
+++ DracoDice.java
@@ -4,7 +4,17 @@ public class DracoDice
 	{
 		System.out.println("DracoDice!");
 
-		Die test = new Die('g');
-		for(int i = 0; i < 20; i++) System.out.println(i + ": " + test.roll());
+		Die gold   = new Die('g');
+		Die silver = new Die('s');
+		Die bronze = new Die('b');
+
+		for(int i = 0; i < 20; i++)
+		{
+			System.out.println("== Ronud " + i + " ==");
+			System.out.println("Gold result  : " + gold.roll());
+			System.out.println("Silver result: " + gold.roll());
+			System.out.println("Bronze result: " + gold.roll());
+			System.out.println();
+		}
 	}
 }
