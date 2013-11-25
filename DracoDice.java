c539c4c0f0a13027c5da44638c0ab736fae98743
Jared <allthosemoments@gmail.com>
2013-11-25 17:47:47 -0600

    rm some debugging

--- DracoDice.java
+++ DracoDice.java
@@ -2,7 +2,7 @@ public class DracoDice
 {
 	public static void main(String[] args)
 	{
-		System.out.println("DracoDice!");
+		System.out.println("== DracoDice! ==");
 
 		Die gold   = new Die('g');
 		Die silver = new Die('s');
@@ -13,15 +13,5 @@ public class DracoDice
 		System.out.print("Drawn from bag : ");
 		bod.take(8);
 		System.out.println("Inside dice bag: " + bod);
-/*
-		for(int i = 0; i < 20; i++)
-		{
-			System.out.println("== Round " + i + " ==");
-			System.out.println("Gold result  : " + gold.roll());
-			System.out.println("Silver result: " + gold.roll());
-			System.out.println("Bronze result: " + gold.roll());
-			System.out.println();
-		}
-*/
 	}
 }
