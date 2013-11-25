ac7e2c1e7f9d635db0dc1b70631625c6e666ea02
Jared <allthosemoments@gmail.com>
2013-11-25 17:41:09 -0600

    adding basic Player methods

--- Player.java
+++ Player.java
@@ -16,4 +16,39 @@ public class Player
 		_active  = new ArrayList<Die> ();
 		_reserve = new ArrayList<Die> ();
 	}
+
+	public void activate(int idx)
+	{
+		_active.add(_hand.remove(idx));
+	}
+
+	public void damage()
+	{
+		_damage++;
+	}
+
+	public void damage(int n)
+	{
+		_damage += n;
+	}
+
+	public ArrayList<Die> discardHand()
+	{
+		ArrayList<Die> temp = new ArrayList<Die> ();
+		for(int i = 0; i < _hand.size(); i++)
+			temp.add(_hand.get(i));
+		_hand.clear();
+		return temp;
+	}
+
+	public void drawNewHand(ArrayList<Die> in)
+	{
+		for(int i = 0; i < in.size(); i++)
+			_hand.add(in.get(i));
+	}
+
+	public void reserve(int idx)
+	{
+		_reserve.add(_hand.remove(idx));
+	}
 }
