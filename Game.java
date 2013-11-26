fde4f54ecfa745b4e2db9b4cb554acdb1b814c8a
Jared <allthosemoments@gmail.com>
2013-11-25 22:00:50 -0600

    no underscores in Game

--- Game.java
+++ Game.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class Game
 {
-	private int _roundNo;
-	private int _playerNo;
-	private int _activePlayer;
-	private Player[] _players;
-	private BagOfDice _diceBag;
+	private int roundNo;
+	private int playerNo;
+	private int activePlayer;
+	private Player[] players;
+	private BagOfDice diceBag;
 
 	Game(int n)
 	{
-		_roundNo      = 0;
-		_playerNo     = n;
-		_activePlayer = 0;
-		_players = new Player[n];
-		_diceBag = new BagOfDice();
+		roundNo      = 0;
+		playerNo     = n;
+		activePlayer = 0;
+		players = new Player[n];
+		diceBag = new BagOfDice();
 
-		for(int i = 0; i < n; i++) _players[i] = new Player();
+		for(int i = 0; i < n; i++) players[i] = new Player();
 
 		boolean gameOn = true;
 		while(gameOn)
@@ -32,8 +32,8 @@ public class Game
 				{
 					printHUD();
 
-					_players[_activePlayer].drawNewHand(_diceBag.take(8));
-					System.out.println(_players[_activePlayer]);
+					players[activePlayer].drawNewHand(diceBag.take(8));
+					System.out.println(players[activePlayer]);
 
 					printCommands();
 
@@ -41,31 +41,38 @@ public class Game
 					char   comm    = command.charAt(0);
 					switch(comm)
 					{
+					case 'a':
+					case 'A':
+					case 'd':
+					case 'D':
 					case 'e':
 					case 'E': endTurn = true; break;
 					case 'q':
 					case 'Q': endTurn = true; endRound = true; gameOn  = false; break;
 					}
 				}
-				_diceBag.replace(_players[_activePlayer].discard());
-				_activePlayer = (++_activePlayer) % _playerNo;
-				if(_activePlayer == 0) endRound = true;
+				diceBag.replace(players[activePlayer].discard());
+				activePlayer = (++activePlayer) % playerNo;
+				if(activePlayer == 0) endRound = true;
 			}
-			_roundNo++;
+			roundNo++;
 		}
 	}
 
 	private void printHUD()
 	{
-		System.out.println("\nRound No. " + _roundNo + 
-			", Player" + (_activePlayer + 1) + "'s turn\n" +
-			"Score: " + _players[_activePlayer].getScore() + "    " +
-			"Damage: " + _players[_activePlayer].getDamage() + "\n");
+		System.out.println("\nRound No. " + roundNo + 
+			", Player" + (activePlayer + 1) + "'s turn\n" +
+			"Score: " + players[activePlayer].getScore() + "    " +
+			"Damage: " + players[activePlayer].getDamage() + "\n");
 	}
 
 	private void printCommands()
 	{
-		System.out.print("Type \'End turn\' to finish your turn\n" + 
+		System.out.print("Commands\n" +
+			"Type \'Activate\' to pick dice to roll\n" +
+			"Type \'Deactivate\' to pick dice to put away\n" +
+			"Type \'End turn\' to finish your turn\n" + 
 			"Type \'Quit game\' to end the game\n" +
 			"-> ");
 	}
