81f4cd8e4a50ff15a163529e6dc5d7ab10bd85a3
Jared <allthosemoments@gmail.com>
2013-11-25 18:32:40 -0600

    bare skeleton of game

--- Game.java
+++ Game.java
@@ -1,16 +1,56 @@
+import java.util.Scanner;
+
 public class Game
 {
-	int _turnNo;
+	int _roundNo;
 	int _playerNo;
+	int _activePlayer;
 	Player[] _players;
 	BagOfDice _diceBag;
 
 	Game(int n)
 	{
-		_turnNo   = 0;
-		_playerNo = n;
+		_roundNo       = 0;
+		_playerNo     = n;
+		_activePlayer = 0;
 		_players = new Player[n];
 		_diceBag = new BagOfDice();
-		System.out.println("Inside the diceBag: " + _diceBag);
+
+		boolean gameOn = true;
+		while(gameOn)
+		{
+			boolean endRound = false;
+			while(!endRound)
+			{
+				Scanner sc = new Scanner(System.in);
+				sc.useDelimiter("\n");
+
+				boolean endTurn = false;
+				while(!endTurn)
+				{
+					System.out.println("Round No. " + _roundNo);
+					System.out.println("Player [" + _activePlayer + "]");
+
+					System.out.print("In the main game section.\n" + 
+						"Type \'End turn\' to finish your turn\n" + 
+						"Type \'Quit game\' to end the game\n" +
+						"-> ");
+
+					String command = sc.next();
+					char   comm    = command.charAt(0);
+					switch(comm)
+					{
+					case 'e':
+					case 'E': endTurn = true; break;
+					case 'q':
+					case 'Q': endTurn = true; endRound = true; gameOn  = false; break;
+					}
+				}
+				_activePlayer = (++_activePlayer) % _playerNo;
+				if(_activePlayer == 0) endRound = true;
+			}
+			_roundNo++;
+		}
 	}
 }
+
